4e02de6a400598953beb504554019af103592a51
Daniil Iakimenko <da.yakimenko@yandex.ru>
2026-03-08 23:07:00 +0300

    feat: fix storage settings v2

--- config/settings.py
+++ config/settings.py
@@ -145,6 +145,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
